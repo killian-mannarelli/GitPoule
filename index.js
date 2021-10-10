0a73ed1b7db4293ab573b1c4d16306d2d01d571b
Killian <killian.mannarelli@outlook.fr>
2021-10-10 23:20:41 +0200

    test bot commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello worldd");
+const Discord = require('discord.js');
+const botClient = new Discord.Client();
+
+botClient.on("message", msg => {
+    if (!msg.content.startsWith("!"))//Si le message ne commence pas par le prefix du config.json
+		return;
+
+    if(msg.author)
+    msg.channel.send('Hello !');
+});
+
+botClient.login('ODk2ODY2MzI4OTA2OTg1NTIy.YWNV2w.mEQcSFgeSJi1B2U3Dl-Pta4ZE9U');
