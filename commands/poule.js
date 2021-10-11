b48a248c1818345924d49f5cafe543e0711cb153
unknown <killian.mannarelli@outlook.fr>
2021-10-11 21:22:46 +0200

    Embeted message update, sending the image properly

--- commands/poule.js
+++ commands/poule.js
@@ -1,10 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('poule')
-		.setDescription('Renvoie une image de la meilleure poule with Pong!'),
+		.setDescription('Renvoie une image de la meilleure poule'),
 	async execute(interaction) {
-		await interaction.reply(`Poule!\nUne image de la poule devrait arriver vite ${interaction.user.username}`);
+		//await interaction.reply(`Poule!\nUne image de la poule devrait arriver vite ${interaction.user.username}`);
+		const exampleEmbed = new MessageEmbed()
+	.setColor('#ffffff')
+	.setTitle('GitPoule')
+	.setDescription('Votez GitPoule')
+	.setImage('https://i.imgur.com/SBbL6ls.png')
+	interaction.reply({ embeds: [exampleEmbed] });
 	},
+	
 };
